a8b01482cd172497b3de8b5e5d035432a17bdda6
Iman Ghasemi Arani <87601518+ImanGhasemiArani@users.noreply.github.com>
2022-11-19 19:13:39 +0330

    refactor sign in screen
    - refactor sign in screen
    - add interactive form to sign in form

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
+import 'package:rive/rive.dart';
 
 import '../assets/assets.gen.dart';
 import '../models/user_model.dart';
@@ -10,32 +11,161 @@ import '../services/api.dart';
 import '../services/local_api.dart';
 import '../services/localization/localization_service.dart';
 import '../services/localization/strs.dart';
+import '../widgets/animations/animation_widget.dart';
 import 'holder_screen.dart';
 import 'sign_up_screen.dart';
 
 RxString eMessage = ''.obs;
 
-class SignInScn extends StatelessWidget {
+class SignInAnimationController {
+  StateMachineController? value;
+  SMIInput<bool>? isChecking;
+  SMIInput<double>? numLook;
+  SMIInput<bool>? isHandsUp;
+  SMIInput<bool>? trigSuccess;
+  SMIInput<bool>? trigFail;
+
+  void dispose() {
+    value?.dispose();
+  }
+}
+
+class SignInScn extends StatefulWidget {
   const SignInScn({super.key});
 
-  static const radius = 13.0;
+  @override
+  State<SignInScn> createState() => _SignInScnState();
+}
+
+class _SignInScnState extends State<SignInScn> {
+  final SignInAnimationController _controller = SignInAnimationController();
+  final List<FocusNode> focusNodes = [FocusNode(), FocusNode()];
+
+  @override
+  void initState() {
+    super.initState();
+    focusNodes[0].addListener(() {
+      _controller.isHandsUp?.change(false);
+      _controller.trigFail?.change(false);
+      _controller.trigSuccess?.change(false);
+      _controller.isChecking?.change(focusNodes[0].hasFocus);
+    });
+    focusNodes[1].addListener(() {
+      _controller.trigFail?.change(false);
+      _controller.trigSuccess?.change(false);
+      _controller.isChecking?.change(false);
+      _controller.isHandsUp?.change(focusNodes[1].hasFocus);
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    focusNodes.forEach((f) => f.dispose());
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     Theme.of(context).brightness == Brightness.dark;
 
-    eMessage = ''.obs;
-    final userModel = UserModel();
-    final formKey = GlobalKey<FormState>();
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 30),
           physics: const BouncingScrollPhysics(),
-          child: Form(
-            key: formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AnimationBuilder(
+                0,
+                0,
+                50,
+                SignInAnimationWidget(controller: _controller),
+              ),
+              AnimationBuilder(
+                1,
+                0,
+                50,
+                SignInForm(controller: _controller, focusNodes: focusNodes),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignInAnimationWidget extends StatelessWidget {
+  const SignInAnimationWidget({
+    super.key,
+    required this.controller,
+  });
+
+  final SignInAnimationController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Transform.scale(
+          scaleX:
+              LocalizationService.textDirection == TextDirection.rtl ? -1 : 1,
+          child: Assets.animations.loginScreenCharacter.rive(
+            fit: BoxFit.fitWidth,
+            stateMachines: ['Login Machine'],
+            onInit: (artBoard) {
+              controller.value = StateMachineController.fromArtboard(
+                artBoard,
+                'Login Machine',
+              );
+              if (controller.value == null) return;
+
+              artBoard.addController(controller.value!);
+              controller.isChecking = controller.value?.findInput('isChecking');
+              controller.numLook = controller.value?.findInput('numLook');
+              controller.isHandsUp = controller.value?.findInput('isHandsUp');
+              controller.trigSuccess =
+                  controller.value?.findInput('trigSuccess');
+              controller.trigFail = controller.value?.findInput('trigFail');
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignInForm extends StatelessWidget {
+  const SignInForm({
+    super.key,
+    required this.controller,
+    required this.focusNodes,
+  });
+
+  final SignInAnimationController controller;
+  final List<FocusNode> focusNodes;
+
+  static const radius = 13.0;
+
+  @override
+  Widget build(BuildContext context) {
+    eMessage = ''.obs;
+    final userModel = UserModel();
+    final formKey = GlobalKey<FormState>();
+    return Card(
+      margin: EdgeInsets.zero,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: formKey,
+          child: AnimationLimiter(
             child: AnimationLimiter(
               child: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: LocalAPI().isShowAnimation
                     ? AnimationConfiguration.toStaggeredList(
                         duration: const Duration(milliseconds: 500),
@@ -56,8 +186,8 @@ class SignInScn extends StatelessWidget {
 
   List<Widget> _getChildren(UserModel userModel, GlobalKey<FormState> formKey) {
     return [
-      _buildTopImg(),
-      const SizedBox(height: 30),
+      const SizedBox(),
+      const SizedBox(),
       _buildSignInWithGoogleBtn(),
       const SizedBox(height: 25),
       _buildDivider(),
@@ -72,14 +202,6 @@ class SignInScn extends StatelessWidget {
       const SizedBox(height: 40),
       _buildAlreadyWarning(),
     ];
-  }
-
-  Widget _buildTopImg() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child:
-          AspectRatio(aspectRatio: 1, child: Assets.images.loginRafiki.svg()),
-    );
   }
 
   Widget _buildUsernameEmailField(UserModel model) {
@@ -99,6 +221,10 @@ class SignInScn extends StatelessWidget {
         return null;
       },
       onSaved: (value) => model.username = model.email = value,
+      focusNode: focusNodes[0],
+      onChanged: (value) {
+        controller.numLook?.change(value.length.toDouble());
+      },
     );
   }
 
@@ -109,6 +235,7 @@ class SignInScn extends StatelessWidget {
       children: [
         Obx(
           () => TextFormField(
+            focusNode: focusNodes[1],
             obscureText: isHide.value,
             enableSuggestions: false,
             autocorrect: false,
@@ -199,17 +326,17 @@ class SignInScn extends StatelessWidget {
         ),
         child: CupertinoButton.filled(
           padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 64),
-          child: Obx(
-            () => isProcessing.isFalse
-                ? text
-                : SizedBox(
-                    height: size.height,
-                    child: FittedBox(
+          child: SizedBox(
+            height: size.height,
+            child: Obx(
+              () => isProcessing.isFalse
+                  ? text
+                  : FittedBox(
                       child: CircularProgressIndicator(
                         color: Get.theme.colorScheme.onPrimary,
                       ),
                     ),
-                  ),
+            ),
           ),
           onPressed: () {
             if (isProcessing.isTrue) return;
@@ -305,19 +432,28 @@ class SignInScn extends StatelessWidget {
   Future<void> _onSignInBtnPressed(GlobalKey<FormState> keyForm,
       UserModel model, RxBool isProcessing) async {
     FocusManager.instance.primaryFocus?.unfocus();
-    await Future.delayed(const Duration(milliseconds: 500));
+    await Future.delayed(const Duration(milliseconds: 1000));
+    controller.isHandsUp?.change(false);
+    controller.isChecking?.change(false);
+    controller.trigSuccess?.change(false);
+    controller.trigFail?.change(false);
+    await Future.delayed(const Duration(milliseconds: 1000));
 
     if (keyForm.currentState?.validate() ?? false) {
       keyForm.currentState?.save();
       try {
         await LocalAPI().setToken(await API().signIn(model));
+        controller.trigSuccess?.change(true);
+        await Future.delayed(const Duration(milliseconds: 3000));
         Get.offAll(const HolderScn());
       } on Exception catch (e) {
+        controller.trigFail?.change(true);
         eMessage.value = '$e';
       } finally {
         isProcessing.value = false;
       }
     } else {
+      controller.trigFail?.change(true);
       isProcessing.value = false;
     }
   }
